2544a996f9da480e4c4c583288eb89d154b4b255
KyleUmney <l.il.kyle@hotmail.co.uk>
2018-08-10 19:50:08 +0100

    Hopefully fixing things

---
+++ Sprites/Cake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Jump.Sprites
+{
+  public class Cake : Sprite
+  {
+    public Cake(Texture2D texture) : base(texture)
+    {
+    }
+  }
+}
