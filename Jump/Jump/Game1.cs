e38958ee2d70f3031234b118ed3b3a108db9a3c1
KyleUmney <l.il.kyle@hotmail.co.uk>
2018-08-10 17:27:22 +0100

    Inital Commit

---
+++ Jump/Jump/Game1.cs
@@ -0,0 +1,138 @@
+﻿using Jump.States;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Jump
+{
+  /// <summary>
+  /// This is the main type for your game.
+  /// </summary>
+  public class Game1 : Game
+  {
+    GraphicsDeviceManager graphics;
+    SpriteBatch spriteBatch;
+
+    public static int screenWidth = 1280;
+    public static int screenHeight = 720;
+
+    private State _currentState;
+    private State _nextState;
+
+    //private float _rotation = 0f;
+    //public Vector2 Position = new Vector2(screenWidth / 2, screenHeight / 2);
+    //public Random rnd;
+    //private float _speed = 10f;
+    //public Texture2D _texture;
+    //public Texture2D _foodTexture;
+    //public Color Colour = Color.Pink;
+
+    //public Vector2 Origin
+    //{
+    //  get
+    //  {
+    //    return new Vector2(_texture.Width / 2, _texture.Height / 2);
+    //  }
+    //}
+    public Game1()
+    {
+      graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
+    }
+
+    /// <summary>
+    /// Allows the game to perform any initialization it needs to before starting to run.
+    /// This is where it can query for any required services and load any non-graphic
+    /// related content.  Calling base.Initialize will enumerate through any components
+    /// and initialize them as well.
+    /// </summary>
+    protected override void Initialize()
+    {
+      // TODO: Add your initialization logic here
+
+      graphics.PreferredBackBufferWidth = screenWidth;
+      graphics.PreferredBackBufferHeight = screenHeight;
+      graphics.ApplyChanges();
+
+      base.Initialize();
+    }
+
+    /// <summary>
+    /// LoadContent will be called once per game and is the place to load
+    /// all of your content.
+    /// </summary>
+    protected override void LoadContent()
+    {
+      // Create a new SpriteBatch, which can be used to draw textures.
+      spriteBatch = new SpriteBatch(GraphicsDevice);
+      _currentState = new GameState(this, Content);
+      _currentState.LoadContent();
+      _nextState = null;
+
+      // TODO: use this.Content to load your game content here
+      //_texture = Content.Load<Texture2D>("Sprites/Player");
+      //_foodTexture = Content.Load<Texture2D>("Sprites/Food");
+    }
+
+    /// <summary>
+    /// UnloadContent will be called once per game and is the place to unload
+    /// game-specific content.
+    /// </summary>
+    protected override void UnloadContent()
+    {
+      // TODO: Unload any non ContentManager content here
+    }
+
+    /// <summary>
+    /// Allows the game to run logic such as updating the world,
+    /// checking for collisions, gathering input, and playing audio.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Update(GameTime gameTime)
+    {
+      if(_nextState != null)
+      {
+        _currentState = _nextState;
+        _currentState.LoadContent();
+        _nextState = null;
+      }
+      _currentState.Update(gameTime);
+      _currentState.PostUpdate(gameTime);
+
+      base.Update(gameTime);
+    }
+
+    public void ChangeState(State state)
+    {
+      _nextState = state;
+    }
+    //protected void Spawn()
+    //{
+    //  Vector2[] _wayPoints = new Vector2[]
+    //  {
+    //    new Vector2(50,50),
+    //    new Vector2(100, 100),
+    //    new Vector2(150, 150),
+    //  };
+
+    // foreach (var point in _wayPoints)
+    //    spriteBatch.Draw(_foodTexture, _wayPoints[0], null, Colour, _rotation, Origin, 1f, SpriteEffects.None, 0.1f);
+    //}
+
+    /// <summary>
+    /// This is called when the game should draw itself.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Draw(GameTime gameTime)
+    {
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+
+      // TODO: Add your drawing code here
+
+      _currentState.Draw(gameTime, spriteBatch);
+
+      base.Draw(gameTime);
+    }
+  }
+}
---
+++ Jump/Jump/Sprites/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Jump.Sprites
+{
+  class Player : Sprite
+  {
+    public Player(Texture2D texture) : base(texture)
+    {
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+      Speed = 5f;
+
+      var direction = new Vector2((float)Math.Cos(_rotation - MathHelper.ToRadians(+90)), (float)Math.Sin(_rotation - MathHelper.ToRadians(+90)));
+
+      _rotation = (float)Math.Atan2(direction.X, direction.Y);
+
+      if (Keyboard.GetState().IsKeyDown(Keys.Up))
+        Position.Y += _rotation * Speed;
+
+      if (Keyboard.GetState().IsKeyDown(Keys.Down))
+        Position.Y -= _rotation * Speed;
+
+      if (Keyboard.GetState().IsKeyDown(Keys.Right))
+        Position.X -= _rotation * Speed;
+
+      if (Keyboard.GetState().IsKeyDown(Keys.Left))
+        Position.X += _rotation * Speed;
+
+
+      Position = Vector2.Clamp(Position, new Vector2(0 + _texture.Width, 0 + _texture.Height), new Vector2(1280 - _texture.Width, 720 - _texture.Height));
+
+      
+
+      base.Update(gameTime);
+    }
+
+  }
+}
