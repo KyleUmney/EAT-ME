0292544459b2b40b188f42b6b79fd15c95c2b0c4
KyleUmney <l.il.kyle@hotmail.co.uk>
2018-08-15 17:16:38 +0100

    Add files via upload

---
+++ Jump/Backend/Managers/UpgradesManager.cs
@@ -0,0 +1,49 @@
+﻿using Backend.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Managers
+{
+  public class UpgradesManager
+  {
+    public List<Upgrade> Upgrades { get; private set; }
+
+    public void LoadUpgrades()
+    {
+      Upgrades = new List<Upgrade>()
+      {
+        new Upgrade()
+        {
+          Name = "Test",
+          StatsModifier = new Stats()
+          {
+            Defence = 1,
+            Health = 1,
+            Speed = 1,
+          }
+        }
+      };
+
+      string jsonFile = JsonConvert.SerializeObject(Upgrades, Formatting.Indented);
+
+      using (var str = new StreamWriter("Content/Upgrades.json"))
+      {
+        str.Write(jsonFile);
+      }
+
+      var fileName = "Content";
+
+      foreach (var file in Directory.GetFiles(fileName, "*.json"))
+      {
+        var serializer = new JsonSerializer();
+        Upgrades = JsonConvert.DeserializeObject<List<Upgrade>>(File.ReadAllText(file),
+              new JsonSerializerSettings() { Formatting = Formatting.Indented });
+      }
+    }
+  }
+}
